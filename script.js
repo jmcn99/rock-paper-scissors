cbf1446a131cf8a4b2f7708dc9e092e940e77a3c
Joseph McNamara <jmcnamara700@gmail.com>
2021-04-25 23:24:01 -0400

    Added GUI

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const buttons = document.querySelectorAll('i')
+
+
+let user;
+let cpu;
+
+buttons.forEach((i) => {
+    i.addEventListener('click', () => {
+        buttonsClear();
+        user = parseInt(i.id);
+        i.style.borderColor = "green";
+        play();
+    });
+});
+
+function buttonsClear() {
+    buttons.forEach((i) => {
+        i.style.borderColor="gray";
+    });
+}
 
 
 function getRandInt(min, max) {
@@ -5,68 +25,107 @@ function getRandInt(min, max) {
 }
 
 function cpuIn() {
-    return getRandInt(1, 3);
+    let num = getRandInt(1, 3);
+    let temp = document.getElementById(num);
+    if(num === user) {
+        temp.setAttribute('style', 'border-top-color: red; border-right-color: red; border-left-color: green; border-bottom-color: green')
+
+    } else {
+        temp.style.borderColor = 'red';
+    }
+    return num;
+    
 }
 
-function point(player) {
-    if(player === "user") {
-        playerScore++;
+function changeSign(num1, num2) {
+    console.log(num1);
+    console.log(num2);
+    console.log("change");
+    if(num1 === 1) {
+        winner.classList.remove('fa-hand-paper', 'fa-hand-scissors');
+        winner.classList.add('fa-hand-rock');
+    } else if(num1 === 2) {
+        winner.classList.remove('fa-hand-rock', 'fa-hand-scissors');
+        winner.classList.add('fa-hand-paper');
     } else {
-        cpuScore++;
+        winner.classList.remove('fa-hand-paper', 'fa-hand-rock');
+        winner.classList.add('fa-hand-scissors');
     }
-    console.log("Score: " + playerScore + ":" + cpuScore + ".");
+    
+    if(num2 === 1) {
+        loser.classList.remove('fa-hand-paper', 'fa-hand-scissors');
+        loser.classList.add('fa-hand-rock');
+    } else if(num2 === 2) {
+        loser.classList.remove('fa-hand-rock', 'fa-hand-scissors');
+        loser.classList.add('fa-hand-paper');
+    } else {
+        loser.classList.remove('fa-hand-paper', 'fa-hand-rock');
+        loser.classList.add('fa-hand-scissors');
+    }
+
 }
+ 
+function point(player) {
+    let winner = document.getElementById("winner");
+    let loser = document.getElementById("loser");
+    if(player === "user") {
+        document.getElementById("greaterthan").innerHTML = ">";
+        document.getElementById("f1").style.borderColor = "green";
+        document.getElementById("f2").style.borderColor = "red";
+        console.log('user score')
+        playerScore++;
+        changeSign(user, cpu);
+    } else if(player === "cpu") {
+        document.getElementById("f2").style.borderColor = "green";
+        document.getElementById("f1").style.borderColor = "red";
+        document.getElementById("greaterthan").innerHTML = ">";
+        console.log("cpu score");
+        cpuScore++;
+        changeSign(cpu, user);
+    } else {
+        document.getElementById("greaterthan").innerHTML = "=";
+        document.getElementById("f1").setAttribute('style', 'border-top-color: red; border-right-color: red; border-left-color: green; border-bottom-color: green');
+        document.getElementById("f2").setAttribute('style', 'border-top-color: red; border-right-color: red; border-left-color: green; border-bottom-color: green');
+        if(cpu === 1) {
+            loser.classList.remove('fa-hand-paper', 'fa-hand-scissors');
+            loser.classList.add('fa-hand-rock');
+            winner.classList.remove('fa-hand-paper', 'fa-hand-scissors');
+            winner.classList.add('fa-hand-rock');
+        } else if(cpu === 2) {
+            loser.classList.remove('fa-hand-rock', 'fa-hand-scissors');
+            loser.classList.add('fa-hand-paper');
+            winner.classList.remove('fa-hand-rock', 'fa-hand-scissors');
+            winner.classList.add('fa-hand-paper');
+        } else {
+            loser.classList.remove('fa-hand-paper', 'fa-hand-rock');
+            loser.classList.add('fa-hand-scissors');
+            winner.classList.remove('fa-hand-paper', 'fa-hand-rock');
+            winner.classList.add('fa-hand-scissors');
+        }
+    }
+}
+
 
 // 1 = rock
 // 2 = paper
 // 3 = scissors
 function play() {
-    let cpu = cpuIn();
-    let user;
-    //do {
-        user = prompt("Please enter '1' for rock, '2' for paper, and '3' for scissors. Good luck!");
-    //} while (user != 1 || user != 2 || user != 3) {
-       // user = prompt("Input not recognized. Please enter '1' for rock, '2' for paper, and '3' for scissors.");
-    //}
-    console.log(user);
-    console.log(cpu);
-    if(user == 1) {
-        if(cpu == 1) {
-            console.log("It's a tie! Both players chose rock.");
-            return null;
-        } else if(cpu == 2) {
-            console.log("CPU wins! Paper beats Rock. ");
-            return point("CPU");
-        } else {
-            console.log("User wins! Rock beats Scissors. ");
-            return point("user");
-        }
-    } else if(user == 2) {
-        if(cpu == 1) {
-            console.log("User wins! Paper beats Rock. ");
-            return point("user");
-        } else if(cpu == 2) {
-            console.log("It's a tie! Both players chose Paper.");
-            return null;
-        } else {
-            console.log("CPU Wins! Scissors beats Paper. ");
-            return point("CPU");
-        }
-    } else if(user == 3) {
-        if(cpu == 1) {
-            console.log("CPU Wins! Rock beats Scissors. ");
-            return point("CPU");
-        } else if(cpu == 2) {
-            console.log("User Wins! Scissors beats Paper. ");
-            return point("user");
-        } else {
-            console.log("It's a tie! Both players chose Scissors.");
-            return null;
-        }
-    } else {
-        console.log("something went wrong...");
-        return null;
+    cpu = cpuIn();
+    
+    //tie
+    if(user === cpu) {
+        point("tie");
+    //user win
+    } else if((user === 1 && cpu === 3) || (user === 2 && cpu === 1) || (user === 3 && cpu === 2)) {
+        console.log("user win");
+        point("user");
+    //cpu win
+    } else if((cpu === 1 && user === 3) || (cpu === 2 && user === 1) || (cpu === 3 && user === 2)) {
+        console.log("cpu win");
+        point("cpu")
     }
+    
+    
 }
 
 
@@ -88,5 +147,5 @@ function playGame() {
 }
 let playerScore = 0;
 let cpuScore = 0;
-playGame();
+
 
